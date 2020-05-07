662cb89ebcd87535e96e3aa3cfff8ad5cbd9b502
Eero L <eero@eerolehtinen.net>
2020-05-07 18:27:16 +0300

    Fixed flashing

--- js/contentScript.js
+++ js/contentScript.js
@@ -26,18 +26,23 @@ function dawdle_block_showTime(timeString) {
         mainDiv.className = "";
     }
 
+    count = 0;
     if (timeoutHandle != undefined) {
         clearTimeout(timeoutHandle);
     }
-    timeoutHandle = setTimeout(dawdle_block_hideTime, 2000)
+    timeoutHandle = setTimeout(dawdle_block_hideTime, 1500);
 }
 
+
+count = 0;
+
 function dawdle_block_hideTime() {
-    if (document.hasFocus() == false) {
-        timeoutHandle = setTimeout(dawdle_block_hideTime, 5000)
+    if (document.hasFocus() == false || count == 0) {
+        timeoutHandle = setTimeout(dawdle_block_hideTime, 1500);
+        count++;
         return;
     }
-    mainDiv = document.getElementById("dawdle_block_annoy")
+    mainDiv = document.getElementById("dawdle_block_annoy");
     if (mainDiv != null) {
         mainDiv.className = "hidden";
     }
