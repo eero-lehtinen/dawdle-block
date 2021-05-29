f990375b30f5c8a9b7b5d76cc94383d78192f0fe
Eero Lehtinen <eero@eerolehtinen.fi>
2021-05-29 07:55:08 +0300

    Add BlockSet object

--- src/scripts/block-set.ts
+++ src/scripts/block-set.ts
@@ -1,12 +1,26 @@
 import { plainToClass } from "class-transformer"
 
-
-export interface BlockRule {
-	type: "asd"
+export interface BlockRuleYt {
+	type: "ytChannel" | "ytCategory"
+	value: {
+		name: string,
+		id: string
+	}
 }
 
+export interface BlockRuleUrl {
+	type: "urlEquals" | "urlContains" | "urlPrefix" | "urlSuffix" | "urlRegexp"
+	value: string
+}
+
+export type BlockRule = BlockRuleUrl | BlockRuleYt
+
+interface ActiveTime { from: number, to: number }
+
+const currentBlockSetVersion = 1
+
 export class BlockSet {
-	readonly v = 1
+	readonly v = currentBlockSetVersion
 	name = "Block set 1"
 	requireActive = false
 	annoyMode = false
@@ -14,14 +28,14 @@ export class BlockSet {
 	resetTime = 0 // milliseconds from midnight
 	lastReset = 0 // millisecods from 1970
 	activeDays: boolean[] = new Array(7).fill(true);
-	activeTime = { from: 0, to: 0 } // milliseconds from midnight
+	activeTime: ActiveTime = { from: 0, to: 0 }// milliseconds from midnight
 	blacklist: BlockRule[] = []
 	whitelist: BlockRule[] = []
 }
 
-export const plainToBlockSet = (obj: any): BlockSet => {
+export const plainToBlockSet = (obj: unknown): BlockSet => {
 
 	// Handle different versions of save data here if needed
 
-	return plainToClass(BlockSet, obj, { excludeExtraneousValues: true })
+	return plainToClass(BlockSet, obj)
 }
--- tests/block-set.spec.ts
+++ tests/block-set.spec.ts
@@ -1,5 +1,36 @@
 import { BlockSet, plainToBlockSet } from "../src/scripts/block-set"
 
-test("Empty js object becomes default BlockSet object", () => {
+test("Empty js object becomes default BlockSet", () => {
 	expect(plainToBlockSet({})).toStrictEqual(new BlockSet())
+})
+
+test("Basic blockset js object converts correctly to BlockSet", () => {
+
+	const testBlockSetObj = {
+		v: 1,
+		name: "Test block set",
+		requireActive: true,
+		annoyMode: true,
+		timeAllowed: 42, // milliseconds
+		resetTime: 42, // milliseconds from midnight
+		lastReset: 42, // millisecods from 1970
+		activeDays: new Array(7).fill(false),
+		activeTime: { from: 42, to: 1337 }, // milliseconds from midnight
+		blacklist: [{ type: "urlEquals" , value: "asdf" }],
+		whitelist: [{ type: "ytChannel", value: { name: "asdf", id: "ID" } }]
+	}
+
+	const testBlockSet = new BlockSet()
+	testBlockSet.name = "Test block set"
+	testBlockSet.requireActive = true
+	testBlockSet.annoyMode = true
+	testBlockSet.timeAllowed = 42
+	testBlockSet.resetTime = 42
+	testBlockSet.lastReset = 42
+	testBlockSet.activeDays = new Array(7).fill(false)
+	testBlockSet.activeTime = { from: 42, to: 1337 }
+	testBlockSet.blacklist = [{ type: "urlEquals" , value: "asdf" }]
+	testBlockSet.whitelist = [{ type: "ytChannel", value: { name: "asdf", id: "ID" } }]
+
+	expect(plainToBlockSet(testBlockSetObj)).toStrictEqual(testBlockSet)
 })
