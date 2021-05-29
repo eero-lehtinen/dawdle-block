f391df94c438982bdfadf3b1fdc7f81271579f45
Eero Lehtinen <eero@eerolehtinen.fi>
2021-05-29 03:25:25 +0300

    Test that decompress works with old stuff

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,10 +1,20 @@
-import { compress, decompress } from "../src/scripts/utils"
+import {compress, decompress} from "../src/scripts/utils"
 
-test("Save data compress and decompress", () => {
-	const testObject = { test: { test: [{}, 42, "test"] } }
+const testObject = {test: {test: [{}, 42, "test"]}}
+
+// testObject compressed with old code
+const testObjectCompressedOld = "H4sIADmFsWAAA6tWKkktLlGygtHR1bU6JkY6EF5sbS0AV4VECSAAAAA="
+
+
+test("Save data compress and decompress loses no data", () => {
+	const testObject = {test: {test: [{}, 42, "test"]}}
 	const compressed = compress(testObject)
 
 	expect(compressed.length).toBeGreaterThan(0)
 	expect(/[A-Za-z0-9+/=]/.test(compressed)).toBe(true)
-	expect(decompress(compressed)).toMatchObject(testObject)
+	expect(decompress(compressed)).toStrictEqual(testObject)
+})
+
+test("Old saves decompress correctly", () => {
+	expect(decompress(testObjectCompressedOld)).toStrictEqual(testObject)
 })
