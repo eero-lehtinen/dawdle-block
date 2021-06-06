279c0c63f92cfe2819bd6d6f3e6f17212fade925
Eero Lehtinen <eero@eerolehtinen.fi>
2021-06-06 21:21:55 +0300

    Split compression tests to be more manageable

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -5,12 +5,14 @@ const testObject = { test: { test: [{}, 42, "test"] } }
 // testObject compressed with old code
 const testObjectCompressedOld = "H4sIADmFsWAAA6tWKkktLlGygtHR1bU6JkY6EF5sbS0AV4VECSAAAAA="
 
-test("Save data compress and decompress loses no data", () => {
-	const testObject = { test: { test: [{}, 42, "test"] } }
+test("Compress output isn't empty and is in base 64", () => {
 	const compressed = compress(testObject)
-
 	expect(compressed.length).toBeGreaterThan(0)
 	expect(/[A-Za-z0-9+/=]/.test(compressed)).toBe(true)
+})
+
+test("Save data compress and decompress loses no data", () => {
+	const compressed = compress(testObject)
 	expect(decompress(compressed)).toStrictEqual(testObject)
 })
 
