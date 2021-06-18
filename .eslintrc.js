4e9449b52c239e4eb9db6e0c95c2c02f8789b16a
Eero Lehtinen <eero@eerolehtinen.fi>
2021-06-18 04:02:45 +0300

    Add eslint args ignore pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,7 +71,7 @@ module.exports = {
 			rules: {
 				"no-unused-vars": "off",
 				"@typescript-eslint/no-unused-vars": ["error",
-					{ varsIgnorePattern: "^_"	}],
+					{ varsIgnorePattern: "^_", argsIgnorePattern: "^_"	}],
 				"no-empty-function": "off",
 				"@typescript-eslint/no-empty-function": ["error", {
 					"allow": ["private-constructors"] }],
