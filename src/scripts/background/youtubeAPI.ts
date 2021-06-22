c392de879000543b559a253d508a5447ef32a207
Eero Lehtinen <eero@eerolehtinen.fi>
2021-06-23 01:31:22 +0300

    Implement youtubeAPI

--- src/scripts/background/youtubeAPI.ts
+++ src/scripts/background/youtubeAPI.ts
@@ -3,33 +3,217 @@ import fetch from "cross-fetch"
 const APIKey = 
 	"A%49%7a%61%53y%42O%4f%6e%39%79%2dbx%42%38%4c%48k%2d%35%51%36%4e%44tq%63y9%5f%46%48%6a%35R%484"
 
-const videoInfoField = "items(snippet(categoryId,channelId))"
+const APIBaseUrl = "https://youtube.googleapis.com/youtube/v3"
 
-interface ytVideoInfo { 
-	channelId: string
-	categoryId: number
+const ytUrl = "www.youtube.com"
+
+interface YtBlockingInfo { 
+	channelId: string | null
+	categoryId: number | null
 }
 
-export const fetchYtVideoInfo = async(videoId: string): Promise<ytVideoInfo> => {
+/**
+ * If url is a page of YouTube channel, video or playlist,
+ * fetch channelId and categoryId associated with it from YouTube data API.
+ * Returns null for categoryId if it can't be determined (e.g. playlist doesn't have a category)
+ * Returns null for both if url is not a channel, video or playlist.
+ * @param url URL with no protocols (e.g. "https://")
+ * @returns blocking results
+ * @throws "Request failed" when YouTube data API is unavailable or returns an error
+ * @throws "Response is empty" when ids are not in found in YouTube databases
+ */
+export const getYtBlockingInfo = async(url: string): Promise<YtBlockingInfo> => {
+	if (!url.startsWith(ytUrl)) {
+		return { channelId: null, categoryId: null }
+	}
+	
+	const videoId = findVideoId(url)
+	if (videoId) {
+		return await fetchVideoBlockingInfo(videoId)
+	}
+
+	const channelId = findChannelId(url)
+	if (channelId) {
+		return { channelId: channelId, categoryId: null }
+	}
+
+	const channelUsername = findChannelUsername(url)
+	if (channelUsername) {
+		return await fetchChannelBlockingInfo(channelUsername)
+	}
+
+	const playlistId = findPlaylistId(url)
+	if (playlistId && !url.includes("playnext=1")) {
+		return await fetchPlaylistBlockingInfo(playlistId)
+	}
+
+	return { channelId: null, categoryId: null }
+}
+
+interface FindBetween {
+	start: string
+	end: string
+}
+
+/**
+ * Returns string between first occurrance of start and end.
+ * If end cannot be found, returns the rest of the source after start.
+ * @param source input string
+ * @param findBetween start and end
+ * @param startPos start search of start from this index
+ * @returns string between start and end
+ */
+const getPartBetween = (source: string, { start, end }: FindBetween, startPos = 0): string | null =>  {
+	const iA = source.indexOf(start, startPos)
+	if (iA === -1)
+		return null
+
+	let iB = source.indexOf(end, iA + start.length)
+	if (iB === -1)
+		iB = source.length
+
+	return source.substring(iA + start.length, iB)
+}
+
+/**
+ * Tests if path is after the www.youtube.com part. 
+ * Then tries to find string between start and end.
+ * Useful when trying to extract e.g. ids from urls.
+ * @param url YouTube url
+ * @param path relevant path (e.g. "/watch?")
+ * @param findBetween start and end, (e.g. for url parameters "param=" and "&")
+ * @returns Identifier found between start and end. Null if identifier can't be found.
+ */
+const findYtUrlIdentifier = (url: string, path: string, { start, end }: FindBetween) => {
+	if (url.startsWith(path, ytUrl.length)) {
+		return getPartBetween(url, { start, end }, ytUrl.length)
+	}
+	return null
+}
+
+/**
+ * Tries to find video id from YouTube url if it exists.
+ * @param url YouTube url
+ * @returns video id if it exists, null otherwise
+ */
+const findVideoId = (url: string): string | null => {
+	return findYtUrlIdentifier(url, "/watch?", { start: "v=", end: "&" })
+}
+
+/**
+ * Tries to find channel id from YouTube url if it exists
+ * @param url YouTube url
+ * @returns channel id if it exists, null otherwise
+ */
+const findChannelId = (url: string): string | null => {
+	return findYtUrlIdentifier(url, "/channel/", { start: "/channel/", end: "/" })
+}
+
+/**
+ * Tries to find channel username from YouTube url if it exists
+ * Supports /c/username and legacy /user/username.
+ * Sometimes usernames are weirdly included in the url (e.g. www.youtube.com/googlecode).
+ * These usernames aren't found with this function. They seem to be very rare 
+ * (maybe only google themselves has them).
+ * @param url YouTube url
+ * @returns channel id if it exists, null otherwise
+ */
+const findChannelUsername = (url: string): string | null => {
+	return findYtUrlIdentifier(url, "/c/", { start: "/c/", end: "/" }) || 
+		findYtUrlIdentifier(url, "/user/", { start: "/user/", end: "/" })
+}
+
+
+/**
+ * Tries to find playlist id from YouTube url if it exists
+ * @param url YouTube url
+ * @returns playlist id if it exists, null otherwise
+ */
+const findPlaylistId = (url: string): string | null => {
+	return findYtUrlIdentifier(url, "/playlist?", { start: "list=", end: "&" })
+}
+
+
+/**
+ * Returns the first item of YouTube data API request response.
+ * Throws errors if results show signs of failure (is empty or status not ok).
+ * @param response fetch response from YouTube data API
+ * @returns first item
+ */
+const getItemFromResponse = 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async(response: Response): Promise<any> => {
+	if (response.status !== 200) {
+		throw new Error("Request failed")
+	}
+	const body = await response.json()
+	if (body.items.length === 0) {
+		throw new Error("Response is empty")
+	}
+
+	return body.items[0]
+}
+
+/**
+ * Fetches channel id of the author of this video and the category id of this video.
+ * @param videoId video id found in the url
+ * @returns channel id and category id
+ */
+const fetchVideoBlockingInfo = async(videoId: string): Promise<YtBlockingInfo> => {
+	const videoInfoField = "items(snippet(categoryId,channelId))"
 	const response = await fetch(
-		"https://youtube.googleapis.com/youtube/v3/videos?part=snippet" +
+		`${APIBaseUrl}/videos?part=snippet` +
 		`&id=${videoId}` + 
 		`&field=${videoInfoField}` +
 		`&key=${APIKey}`,
 	)
 
-	if (response.status !== 200) {
-		throw new Error("Request failed")
-	}
-	
-	const body = await response.json()
-
-	if (body.items.length === 0) {
-		throw new Error("Video not found")
-	}
+	const item = await getItemFromResponse(response)
 
 	return {
-		channelId: body.items[0].snippet.channelId, 
-		categoryId: body.items[0].snippet.categoryId, 
+		channelId: item.snippet.channelId, 
+		categoryId: item.snippet.categoryId, 
+	}
+}
+
+/**
+ * Fetches channel id of the author of this playlist.
+ * @param playlistId playlist id found in the url
+ * @returns channel id
+ */
+const fetchPlaylistBlockingInfo = async(playlistId: string): Promise<YtBlockingInfo> => {
+	const playlistInfoField = "items/snippet/channelId"
+	const response = await fetch(
+		`${APIBaseUrl}/playlists?part=snippet` +
+		`&id=${playlistId}` + 
+		`&field=${playlistInfoField}` +
+		`&key=${APIKey}`,
+	)
+
+	const item = await getItemFromResponse(response)
+
+	return {
+		channelId: item.snippet.channelId,
+		categoryId: null, 
+	}
+}
+
+/**
+ * Fetches channel id associated with this username,
+ * @param username username found in the url
+ * @returns channel id
+ */
+const fetchChannelBlockingInfo = async(username: string): Promise<YtBlockingInfo> => {
+	const response = await fetch(
+		`${APIBaseUrl}/channels?part=id` +
+		`&forUsername=${username}` +
+		`&key=${APIKey}`,
+	)
+
+	const item = await getItemFromResponse(response)
+
+	return {
+		channelId: item.id,
+		categoryId: null, 
 	}
 }
--- tests/youtubeAPI.test.ts
+++ tests/youtubeAPI.test.ts
@@ -1,39 +1,118 @@
 import { mocked } from "ts-jest/utils"
 import fetch from "cross-fetch"
-import { fetchYtVideoInfo } from "../src/scripts/background/youtubeAPI"
+import { getYtBlockingInfo } from "../src/scripts/background/youtubeAPI"
 
 jest.mock("cross-fetch")
 
 const mockedFetch = mocked(fetch, true)
 
-describe("test youtube api fetching", () => {
+const getLatestFetchParams = () => mockedFetch.mock.calls[mockedFetch.mock.calls.length - 1]
+
+describe("test YouTube API error states", () => {
+	it("returns null values when YouTube isn't in the beginning of the url", async() => {
+		await expect(getYtBlockingInfo("asd")).resolves.toStrictEqual({ channelId: null, categoryId: null })
+		await expect(getYtBlockingInfo("asdwww.youtube.com")).resolves.toStrictEqual({ channelId: null, categoryId: null })
+	})
+
+	it("returns null values when YouTube is not on channel, playlist or video page", async() => {
+		await expect(getYtBlockingInfo("www.youtube.com/feed/subscriptions"))
+			.resolves.toStrictEqual({ channelId: null, categoryId: null })
+	})
+
+	it("returns null values when YouTube is on a valid page but id can't be located in url", async() => {
+		// No "v=" included, which we use to locate the id
+		await expect(getYtBlockingInfo("www.youtube.com/watch?asdfasdf&t=42"))
+			.resolves.toStrictEqual({ channelId: null, categoryId: null })
+	})
+	
+	it("throws 'Response is empty' error when channel with this id isn't found", async() => {
+		mockedFetch.mockResolvedValueOnce({
+			status: 200, 
+			json: async() => {return { items: [] }},	
+		} as Response)
+
+		await expect(getYtBlockingInfo("www.youtube.com/watch?v=asd")).rejects.toThrowError("Response is empty")
+	})
+
+	it("throws 'Request failed' error when request status is not 200", async() => {
+		mockedFetch.mockResolvedValueOnce({
+			status: 404, 
+		} as Response)
+		
+		await expect(getYtBlockingInfo("www.youtube.com/watch?v=asd")).rejects.toThrowError("Request failed")
+	})
+})
+
+describe("test YouTube API blocking info for video urls", () => {
 	it("returns correct results with a valid id", async() => {
-		mockedFetch.mockResolvedValue({
+		mockedFetch.mockResolvedValueOnce({
 			status: 200, 
 			json: async() => {
 				return { items: [{ snippet: { channelId: "UC_x5XG1OV2P6uZZ5FSM9Ttw", categoryId: "22" } }] }
 			}, 
 		} as Response)
 		
-		const res = await fetchYtVideoInfo("ylLzyHk54Z0")
+		const res = await getYtBlockingInfo("www.youtube.com/watch?v=ylLzyHk54Z0&t=42")
+		expect(getLatestFetchParams()?.[0]).toContain("videos")
+		expect(getLatestFetchParams()?.[0]).toContain("part=snippet")
+		expect(getLatestFetchParams()?.[0]).toContain("id=ylLzyHk54Z0")
 		expect(res.channelId).toStrictEqual("UC_x5XG1OV2P6uZZ5FSM9Ttw")
 		expect(res.categoryId).toStrictEqual("22")
 	})
+})
 
-	it("throws 'Video not found' error when id is invalid", async() => {
-		mockedFetch.mockResolvedValue({
-			status: 200, 
-			json: async() => {return { items: [] }},	
-		} as Response)
 
-		await expect(fetchYtVideoInfo("")).rejects.toThrowError("Video not found")
+
+describe("test YouTube API blocking info for channel urls", () => {
+	it("returns correct results with a valid id", async() => {
+		const res = await getYtBlockingInfo("www.youtube.com/channel/UC_x5XG1OV2P6uZZ5FSM9Ttw")
+		expect(res).toStrictEqual({ channelId: "UC_x5XG1OV2P6uZZ5FSM9Ttw", categoryId: null })
 	})
 
-	it("throws 'Request failed' error when request status is not 200", async() => {
-		mockedFetch.mockResolvedValue({
-			status: 404, 
+	const testUserName = async(url: string) => {
+		mockedFetch.mockResolvedValueOnce({
+			status: 200, 
+			json: async() => {
+				return { items: [{ id: "UCK8sQmJBp8GCxrOtXWBpyEA" }] }
+			}, 
 		} as Response)
-		
-		await expect(fetchYtVideoInfo("asd")).rejects.toThrowError("Request failed")
+
+		const res = await getYtBlockingInfo(url)
+
+		expect(getLatestFetchParams()?.[0]).toContain("channels")
+		expect(getLatestFetchParams()?.[0]).toContain("part=id")
+		expect(getLatestFetchParams()?.[0]).toContain("forUsername=google")
+		expect(res).toStrictEqual({ channelId: "UCK8sQmJBp8GCxrOtXWBpyEA", categoryId: null })
+	}
+
+	it("returns correct results with a valid username", async() => {
+		await testUserName("www.youtube.com/c/google")
+	})
+
+	it("returns correct results with a valid legacy username", async() => {
+		await testUserName("www.youtube.com/user/google")
+	})
+})
+
+describe("test YouTube API blocking info for playlist urls", () => {
+	it("returns correct results with a valid id", async() => {
+		mockedFetch.mockResolvedValueOnce({
+			status: 200, 
+			json: async() => {
+				return { items: [{ snippet: { channelId: "UC_x5XG1OV2P6uZZ5FSM9Ttw" } }] }
+			}, 
+		} as Response)
+
+		const res = await getYtBlockingInfo("www.youtube.com/playlist?list=PLOU2XLYxmsIJs-bCAsrT21mTgen_DklG1")
+
+		expect(getLatestFetchParams()?.[0]).toContain("playlists")
+		expect(getLatestFetchParams()?.[0]).toContain("part=snippet")
+		expect(getLatestFetchParams()?.[0]).toContain("id=PLOU2XLYxmsIJs-bCAsrT21mTgen_DklG1")
+		expect(res).toStrictEqual({ channelId: "UC_x5XG1OV2P6uZZ5FSM9Ttw", categoryId: null })
+	})
+
+	it("returns empty results when url contains 'playnext=1'", async() => {
+		const res = await getYtBlockingInfo("www.youtube.com/playlist?list=asd&playnext=1")
+		expect(res).toStrictEqual({ channelId: null, categoryId: null })
 	})
 })
