275734cc05b3e64f3aeb1409fd29921858aeca47
Eero Lehtinen <eero@eerolehtinen.fi>
2021-06-25 07:24:29 +0300

    Fix timeUtil to have milliseconds

--- src/scripts/background/timeUtils.ts
+++ src/scripts/background/timeUtils.ts
@@ -1,2 +1,3 @@
 export const timeToMSSinceMidnight = (time: Date): number =>
-	time.getSeconds() * 1000 + time.getMinutes() * 60000 + time.getHours() * 3600000
+	time.getMilliseconds() + time.getSeconds() * 1000 + 
+		time.getMinutes() * 60000 + time.getHours() * 3600000
