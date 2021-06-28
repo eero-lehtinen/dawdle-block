0af966fdfeedaf5ceaa2ba13ecb4520bb8f82034
Eero Lehtinen <eero@eerolehtinen.fi>
2021-06-29 01:05:57 +0300

    Add observer class

---
+++ src/background/observer.ts
@@ -0,0 +1,30 @@
+
+
+export type Listener<EventType> = (ev: EventType) => void
+
+/**
+ * Class used for publishing events to listeners.
+ */
+export class Observer<EventType> {
+	private listeners: Listener<EventType>[] = []
+
+	/**
+	 * Subscribes listener to receive events in the future.
+	 * @param listener function for receiving events
+	 * @returns unsubscribe function
+	 */
+	subscribe(listener: Listener<EventType>): () => void {
+		this.listeners.push(listener)
+		return () => {
+			this.listeners = this.listeners.filter(l => l !== listener)
+		}
+	}
+
+	/**
+	 * Publishes event to all listeners
+	 * @param event 
+	 */
+	publish(event: EventType): void {
+		this.listeners.forEach(listener => listener(event))
+	}
+}
---
+++ tests/observer.test.ts
@@ -0,0 +1,19 @@
+import { Observer, Listener } from "../src/background/observer"
+
+describe("test observer", () => {
+	it("can subscribe, receive events and unsubscribe", () => {
+		const observer = new Observer<number>()
+		
+		const listener: Listener<number> = jest.fn()
+		const unsubscribe = observer.subscribe(listener)
+
+		observer.publish(42)
+		expect(listener).toBeCalledWith(42)
+		expect(listener).toBeCalledTimes(1)
+
+		unsubscribe()
+
+		observer.publish(24)
+		expect(listener).toBeCalledTimes(1)
+	})
+})
