ebb317da9173f89153aea1d6303ec8579750fb95
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-02 23:03:31 +0300

    Test sleep function

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -9,3 +9,21 @@ describe("test time to ms since midnight", () => {
 		expect(timeToMSSinceMidnight(new Date("2000-01-01T05:00:00"))).toStrictEqual(5 * 3600000)
 	})
 })
+
+describe("test sleep", () => {
+	beforeEach(() => { jest.useFakeTimers() })
+	afterEach(() => { jest.useRealTimers() })
+
+	it("waits milliseconds based on parameters, then resolves", async() => {
+		const spy = jest.fn()
+		void sleep(100).then(spy)
+
+		jest.advanceTimersByTime(80)
+		await Promise.resolve()
+		expect(spy).not.toHaveBeenCalled()
+
+		jest.advanceTimersByTime(20)
+		await Promise.resolve()
+		expect(spy).toHaveBeenCalled()
+	})
+})
