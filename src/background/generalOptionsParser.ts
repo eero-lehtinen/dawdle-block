c070080a8b0271bb603490496af77b4af1528cea
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-02 18:44:23 +0300

    Add general options v1

--- src/background/generalOptionsParser.ts
+++ src/background/generalOptionsParser.ts
@@ -4,23 +4,90 @@ import { z } from "zod"
 const zClockType = z.union([z.literal(12), z.literal(24)]).default(24)
 const zSettingsProtection = z.enum(["never", "always", "timerZero"]).default("never")
 
-const zGeneralOptionsV1 = z.object({
-	v: z.union([z.undefined(), z.literal(1)]).transform((): 1 => 1),
+const zGeneralOptionsDataV0 = z.object({
+	v: z.union([z.undefined(), z.literal(0)]).transform((): 0 => 0),
 	clockType: zClockType,
 	displayHelp: z.boolean().default(true),
 	darkTheme: z.boolean().default(false),
 	settingProtection: zSettingsProtection,
 	typingTestWordCount: z.number().int().default(30),
-}).default({})
+})
 
-export type GeneralOptions = z.infer<typeof zGeneralOptionsV1>
+type GeneralOptionsDataV0 = z.infer<typeof zGeneralOptionsDataV0>
 
-export const plainToGeneralOptions = 
+const zTheme = z.enum(["system", "dark", "light"]).default("system")
+
+const zGeneralOptionsDataV1 = zGeneralOptionsDataV0
+	.omit({ darkTheme: true })
+	.extend({
+		v: z.literal(1),
+		theme: zTheme,
+	})
+
+type GeneralOptionsDataV1 = z.infer<typeof zGeneralOptionsDataV1>
+
+export type GeneralOptionsData = GeneralOptionsDataV1
+
+/**
+ * Converts plain js object into GeneralOptions with type validation
+ * @throws {Error} if object is not parseable
+ * @param obj
+ * @returns 
+ */
+export const plainToGeneralOptionsData = 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-(obj: any): GeneralOptions => {
-	return zGeneralOptionsV1.parse(obj)
+(obj: any): GeneralOptionsData => {
+	if (obj !== null && obj !== undefined) {
+		if (parseableV0(obj)) {
+			const parsedGeneralOptionsV0 = zGeneralOptionsDataV0.parse(obj)
+			return convertV0toV1(parsedGeneralOptionsV0)
+		}
+		else if (parseableV1(obj)) {
+			return zGeneralOptionsDataV1.parse(obj)
+		}
+	}
+	throw new Error("Can't parse to general options")
 }
 
-export const createDefaultGeneralOptions = (): GeneralOptions => {
-	return zGeneralOptionsV1.parse({ v: 1 })
+export const createDefaultGeneralOptionsData = (): GeneralOptionsData => {
+	return zGeneralOptionsDataV1.parse({ v: 1 })
+}
+
+/**
+ * Converts block set version 0 to version 1.
+ * In practice we replace some block rules to the new pattern style.
+ * @param generalOptions 
+ * @returns 
+ */
+const convertV0toV1 = (generalOptionsV0: GeneralOptionsDataV0) => {
+
+	const { darkTheme, ...sharedParams } = generalOptionsV0
+
+	const generalOptionsV1: GeneralOptionsDataV1 = {
+		...sharedParams, 
+		v: 1,
+		theme: darkTheme ? "dark" : "system", 
+	}
+
+	return generalOptionsV1
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Checks if passed object should be parsed with v0 block set parser
+ * @param obj 
+ * @returns 
+ */
+const parseableV0 = (obj: any) => {
+	return typeof obj === "object" && (!("v" in obj) || obj.v < 1)
+}
+
+/**
+ * Checks if passed object should be parsed with v1 block set parser
+ * @param obj 
+ * @returns 
+ */
+const parseableV1 = (obj: any) => {
+	return typeof obj === "object" && "v" in obj && obj.v === 1
 }
--- tests/generalOptionsParser.test.ts
+++ tests/generalOptionsParser.test.ts
@@ -1,45 +1,42 @@
-import { plainToGeneralOptions, createDefaultGeneralOptions } 
+import { plainToGeneralOptionsData, createDefaultGeneralOptionsData, GeneralOptionsData } 
 	from "../src/background/generalOptionsParser"
 
 describe("test GeneralOptions parsing", () => {
-	const defaultGeneralOptions = createDefaultGeneralOptions()
+	const defaultGeneralOptions = createDefaultGeneralOptionsData()
 
 	it("basic example is parsed", () => {
 		const testOptionsObj = {
 			v: 1,
 			clockType: 12,
 			displayHelp: false,
-			darkTheme: true,
+			theme: "light",
 			settingProtection: "always",
 			typingTestWordCount: 100,
 		}
 
-		expect(plainToGeneralOptions(testOptionsObj)).toStrictEqual(testOptionsObj)
+		expect(plainToGeneralOptionsData(testOptionsObj)).toStrictEqual(testOptionsObj)
 	})
 
 	it("non-objects throw", () => {
-		expect(() => { plainToGeneralOptions("string") }).toThrow()
-		expect(() => { plainToGeneralOptions(42) }).toThrow()
-		expect(() => { plainToGeneralOptions(() => {return 0}) }).toThrow()
-		expect(() => { plainToGeneralOptions(null)}).toThrow()
+		expect(() => { plainToGeneralOptionsData("string") }).toThrow()
+		expect(() => { plainToGeneralOptionsData(42) }).toThrow()
+		expect(() => { plainToGeneralOptionsData(() => {return 0}) }).toThrow()
+		expect(() => { plainToGeneralOptionsData(null)}).toThrow()
 	})
 
 	it("objects with members of invalid types throw", () => {
 
 		const testOptionsObj = {
+			v: 1,
 			clockType: "string", 
-			darkTheme: undefined,
-		}
+			theme: undefined,
+		} 
 
-		expect(() => { plainToGeneralOptions(testOptionsObj)}).toThrow()
+		expect(() => { plainToGeneralOptionsData(testOptionsObj)}).toThrow()
 	})
 
 	it("incomplete objects get filled with defaults", () => {
-		expect(plainToGeneralOptions({})).toStrictEqual(defaultGeneralOptions)
-	})
-
-	it("undefined creates a default general options", () => {
-		expect(plainToGeneralOptions(undefined)).toStrictEqual(defaultGeneralOptions)
+		expect(plainToGeneralOptionsData({})).toStrictEqual(defaultGeneralOptions)
 	})
 
 	it("objects retain their valid property names and lose invalid ones", () => {
@@ -49,9 +46,41 @@ describe("test GeneralOptions parsing", () => {
 			loseMe: "lost",
 		}
 
-		const generalOptions = plainToGeneralOptions(testBlockSetObj)
+		const generalOptions = plainToGeneralOptionsData(testBlockSetObj)
 
 		expect(generalOptions).toHaveProperty("displayHelp")
 		expect(generalOptions).not.toHaveProperty("loseMe")
+	})
+
+	
+	it("incomplete objects get filled with defaults", () => {
+		expect(plainToGeneralOptionsData({})).toStrictEqual(defaultGeneralOptions)
+	})
+
+	it("V0 default theme (darkTheme=false) gets converted to default theme in V1", () => {
+		const testBlockSetObj = {
+			darkTheme: false,
+		}
+
+		const testBlockSetObjResult: GeneralOptionsData = {
+			...defaultGeneralOptions,
+			theme: "system",
+		}
+
+		expect(plainToGeneralOptionsData(testBlockSetObj)).toStrictEqual(testBlockSetObjResult)
+	})
+
+	
+	it("V0 non-default theme (darkTheme=true) gets converted to dark theme in V1", () => {
+		const testBlockSetObj = {
+			darkTheme: true,
+		}
+
+		const testBlockSetObjResult: GeneralOptionsData = {
+			...defaultGeneralOptions,
+			theme: "dark",
+		}
+
+		expect(plainToGeneralOptionsData(testBlockSetObj)).toStrictEqual(testBlockSetObjResult)
 	})
 })
