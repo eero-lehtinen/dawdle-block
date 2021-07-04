9416f3baf6538e23d546441005c21e7ee943320f
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-04 16:16:14 +0300

    Use straight ts-jest instead of babel for testing

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
 	preset: "ts-jest",
 	collectCoverage: true,
 	collectCoverageFrom: ["./src/**/*.ts"],
-	transform: {
-		"\\.(ts|js)x?$": "ts-jest",
-	},
-	moduleFileExtensions: ["ts", "js"],
 	setupFilesAfterEnv: ["./tests/setup.ts"],
 }
