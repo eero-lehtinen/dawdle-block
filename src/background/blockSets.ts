4251a8a83b615fc527332fe6e96e38d6ad43064f
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-13 16:18:01 +0300

    Implement block set addition

--- src/background/blockSets.ts
+++ src/background/blockSets.ts
@@ -1,6 +1,6 @@
 import { BlockSet, BlockTestRes } from "./blockSet"
 import { timeToMSSinceMidnight } from "../shared/utils"
-import { BlockSetStorage } from "./blockSetStorage"
+import { BrowserStorage } from "./browserStorage"
 
 /**
  * Loads blocksets from storage, maintains synchronization when modified. 
@@ -8,15 +8,11 @@ import { BlockSetStorage } from "./blockSetStorage"
  */
 export class BlockSets {
 
-	private _blockSets: BlockSet[] = []
-	private blockSetStorage: BlockSetStorage
-
-	get blockSets(): BlockSet[] {
-		return this._blockSets
-	}
+	private _list: BlockSet[] = []
+	private browserStorage: BrowserStorage
 
 	private constructor() {
-		this.blockSetStorage = new BlockSetStorage({ preferSync: true })
+		this.browserStorage = new BrowserStorage({ preferSync: true })
 	}
 
 	/**
@@ -26,7 +22,10 @@ export class BlockSets {
 	 */
 	static async create(): Promise<BlockSets> {
 		const instance = new BlockSets()
-		instance._blockSets = await instance.blockSetStorage.loadBlockSets()
+		instance._list = await instance.browserStorage.loadBlockSets()
+		// Create a single default block set if storage is empty
+		if (instance._list.length === 0) 
+			instance._list = [new BlockSet(0)]
 		return instance
 	}
 
@@ -42,7 +41,7 @@ export class BlockSets {
 		const now = new Date()
 		const msSinceMidnight = timeToMSSinceMidnight(now)
 		const weekDay = now.getDay()
-		for (const blockSet of this._blockSets) {
+		for (const blockSet of this._list) {
 			// if today is not an active day or not in active hours
 			if (!blockSet.isInActiveWeekday(weekDay) || !blockSet.isInActiveTime(msSinceMidnight)) 
 				continue
@@ -54,5 +53,95 @@ export class BlockSets {
 			}
 		}
 		return blockingBSIds
+	}
+
+	get list(): BlockSet[] {
+		return this._list
+	}
+
+	/** Returns list of block set ids in the order they should appear in UI */
+	getIds(): number[] {
+		return this._list.map((bs) => bs.id)
+	}
+
+	/**
+	 * Add a new block set to the end of the list with default values.
+	 * Id will be automatically assigned.
+	 * @returns added block set
+	 * @throws "Can't save item, it is too large" when size quota is exceeded
+	 * @throws "Can't save item, too many write operations" when write operations quota is exceeded
+	 */
+	async addDefaultBlockSet(): Promise<BlockSet> {
+		const newBlockSet = new BlockSet(this.findNextSafeId())
+		newBlockSet.name = this.computeNewName()
+		await this.browserStorage.saveNewBlockSet(newBlockSet, [...this.getIds(), newBlockSet.id])
+		this._list.push(newBlockSet)
+		return newBlockSet
+	}
+
+	/**
+	 * Add a new block set to the end of the list with values copied from copyFrom.
+	 * timeElapsed will be set to zero and id will be automatically assigned.
+	 * (copy) will be appended to the end of the name.
+	 * @param copyFrom block set to get values from
+	 * @returns added block set
+	 * @throws "Can't save item, it is too large" when size quota is exceeded
+	 * @throws "Can't save item, too many write operations" when write operations quota is exceeded
+	 */
+	async addBlockSetCopy(copyFrom: BlockSet): Promise<BlockSet> {
+		const newBlockSet = new BlockSet(this.findNextSafeId(), copyFrom.data)
+		newBlockSet.name = this.computeCopyName(copyFrom.name)
+		await this.browserStorage.saveNewBlockSet(newBlockSet, [...this.getIds(), newBlockSet.id])
+		this._list.push(newBlockSet)
+		return newBlockSet
+	}
+
+	/** Returns the smallest block set id that is >= 0 and unused. */
+	private findNextSafeId() {
+		const currentIds = this.getIds()
+		let nextSafeId = 0
+		while(currentIds.indexOf(nextSafeId) > -1) {
+			nextSafeId += 1
+		}
+		return nextSafeId
+	}
+
+	/**
+	 * Returns copy oldName with "(copy)" appended.
+	 * If oldName already ends with "(copy)", then replace it with "(copy2x)", then "(copy3x)".
+	 * Continues in this fashion for further copies.
+	 * @param oldName 
+	 */
+	private computeCopyName(oldName: string): string {
+		const regexRes = /\(copy(?:(\d{1,})x)?\)$/.exec(oldName)
+		if (regexRes === null) {
+			return `${ oldName } (copy)`
+		}
+		
+		const copyNumberStr = regexRes[1]
+		const copyIndex = regexRes.index
+		const copyNumber = copyNumberStr !== undefined ? parseInt(copyNumberStr, 10) : 1
+
+		return `${oldName.substring(0, copyIndex)}(copy${copyNumber + 1}x)`
+	}
+
+	
+	/**
+	 * Returns "Block Set ${number larger than any that already exist}", starting from 1
+	 * E.g. if there exists block set with name "Block Set 7", 
+	 * and no larger numbers exist, then this will return "Block Set 8".
+	 * In that case 1 will not be chosen, even if it is technically "free".
+	 */
+	private computeNewName(): string {
+		let largestNumber = 0
+		for (const blockSet of this._list) {
+			const regexRes = /^Block Set (\d{1,})$/.exec(blockSet.name)
+			if (regexRes !== null) {
+				const number = parseInt(regexRes[1] as string, 10)
+				largestNumber = Math.max(largestNumber, number)
+			}
+		}
+
+		return `Block Set ${largestNumber + 1}`
 	}
 }
--- tests/background/blockSets.test.ts
+++ tests/background/blockSets.test.ts
@@ -1,22 +1,119 @@
 import { BlockSet } from "@src/background/blockSet"
 import { BlockSets } from "@src/background/blockSets"
-import { BlockSetStorage } from "@src/background/blockSetStorage"
+import { BrowserStorage } from "@src/background/browserStorage"
 import { mocked } from "ts-jest/utils"
 
-jest.mock("@src/background/blockSetStorage")
+jest.mock("@src/background/browserStorage")
 
-const blockSetStorageMock = mocked(BlockSetStorage, true)
+const browserStorageMock = mocked(BrowserStorage, true)
 
-describe("test BlockSets", () => {
+describe("test BlockSets construction", () => {
 	it("loads block sets from block set storage", async() => {
-		blockSetStorageMock.prototype.loadBlockSets
+		browserStorageMock.prototype.loadBlockSets
 			.mockImplementation(async() => Promise.resolve([new BlockSet(42)]))
 
 		const blockSets = await BlockSets.create()
-		expect(blockSetStorageMock).toBeCalledTimes(1)
-		expect(blockSets.blockSets).toStrictEqual([new BlockSet(42)])
+		expect(browserStorageMock).toBeCalledTimes(1)
+		expect(blockSets.list).toStrictEqual([new BlockSet(42)])
 	})
 	
-	it.todo("creates a default block set when storage is empty")
+	it("creates a default block set when storage is empty", async() => {
+		browserStorageMock.prototype.loadBlockSets
+			.mockImplementation(async() => Promise.resolve([]))
+		
+		const blockSets = await BlockSets.create()
+		expect(blockSets.list).toStrictEqual([new BlockSet(0)])
+	})
+})
+
+describe("test BlockSets methods", () => {
+	let blockSets: BlockSets
+	beforeEach(async() => {
+		browserStorageMock.prototype.loadBlockSets
+			.mockImplementation(async() => Promise.resolve([new BlockSet(0, { name: "TEST" })]))
+		blockSets = await BlockSets.create()
+	})
+
+	// New block sets should get saved to storage.
+	// They should be assigned a non overlapping id.
+	// Name should be "Block Set ${number}", 
+	// number is based on the amount of old block sets starting with "Block Set".
+	it("can add new default block sets", async() => {
+		const newBlockSet = await blockSets.addDefaultBlockSet()
+		const bsIds = blockSets.getIds()
+
+		// Check for duplicate ids
+		expect(new Set(bsIds).size).toBe(bsIds.length) 
+		// Added block set is equal to default block set except from id and name
+		const defaultBlockSet = new BlockSet(newBlockSet.id)
+		defaultBlockSet.name = newBlockSet.name
+		expect(newBlockSet).toMatchObject(defaultBlockSet)
+
+		expect(blockSets.list).toContain(newBlockSet)
+		expect(browserStorageMock.prototype.saveNewBlockSet)
+			.toBeCalledWith(newBlockSet, blockSets.getIds())
+	})
+
+	it("added new block set name is 'Block Set ${number}' with restrictions", async() => {
+		let newBlockSet = await blockSets.addDefaultBlockSet()
+		expect(newBlockSet.name).toBe("Block Set 1")
+		newBlockSet = await blockSets.addDefaultBlockSet()
+		expect(newBlockSet.name).toBe("Block Set 2")
+
+		newBlockSet.name = "Block Set 120"
+		newBlockSet = await blockSets.addDefaultBlockSet()
+		expect(newBlockSet.name).toBe("Block Set 121")
+	})
+
+	// Copied block sets should get saved to storage.
+	// They should be assigned a non overlapping id.
+	// Their name should have (copy) appended to the end.
+	// If old name had "(copy)" already, then names should continue (copy2), (copy3) etc.
+	it("can add new copies of block sets", async() => {
+		const testBlockSet = new BlockSet(100, { name: "TEST" })
+		const newBlockSet = await blockSets.addBlockSetCopy(testBlockSet)
+		const bsIds = blockSets.getIds()
+
+		// Check for duplicate ids
+		expect(new Set(bsIds).size).toBe(bsIds.length)
+
+		// Added block set is equal to copied block set except from id, name and time elapsed
+		expect(newBlockSet.data).toStrictEqual({ ...testBlockSet.data, name: "TEST (copy)" })
+		expect(newBlockSet.timeElapsed).toStrictEqual(0)
+
+		expect(blockSets.list).toContain(newBlockSet)
+		expect(browserStorageMock.prototype.saveNewBlockSet)
+			.toBeCalledWith(newBlockSet, blockSets.getIds())
+	})
+
+	it("added new block set copy name is '${copyName} (copy${number}x)' " +
+	"with restrictions", async() => {
+		const testBlockSet = new BlockSet(100, { name: "TEST" })
+		let newBlockSet = await blockSets.addBlockSetCopy(testBlockSet)
+		expect(newBlockSet.name).toStrictEqual(`${testBlockSet.name} (copy)`)
+		newBlockSet = await blockSets.addBlockSetCopy(testBlockSet)
+		expect(newBlockSet.name).toStrictEqual(`${testBlockSet.name} (copy)`)
+
+		newBlockSet = await blockSets.addBlockSetCopy(newBlockSet)
+		expect(newBlockSet.name).toStrictEqual(`${testBlockSet.name} (copy2x)`)
+
+		newBlockSet = await blockSets.addBlockSetCopy(newBlockSet)
+		expect(newBlockSet.name).toStrictEqual(`${testBlockSet.name} (copy3x)`)
+
+		newBlockSet.name = "TEST (copy123x)"
+		newBlockSet = await blockSets.addBlockSetCopy(newBlockSet)
+		expect(newBlockSet.name).toStrictEqual(`${testBlockSet.name} (copy124x)`)
+		
+	})
+})
+
+
+
+describe("test BlockSets blockedBy method", () => {
+	beforeEach(() => {
+		browserStorageMock.prototype.loadBlockSets
+			.mockImplementation(async() => Promise.resolve([]))
+	})
+
 	it.todo("returns ids of each matching block set")
 })
