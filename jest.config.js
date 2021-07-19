ad5be32c01739f045bfefe3d8b8e7afb6f26c06e
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-19 21:42:16 +0300

    Disable coverage from UI files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	verbose: true,
 	collectCoverage: true,
 	maxWorkers: 1,
-	collectCoverageFrom: ["./src/**/*.{ts,tsx}"],
+	collectCoverageFrom: ["./src/**/*.ts"],
 	moduleNameMapper: {
 		"^react$": "preact/compat",
 		"^react-dom/test-utils$": "preact/test-utils",
