d392bd2d810d66d19b2c3c636f594822974c19b8
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-25 23:12:35 +0300

    Set popup button to open options menu

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,10 @@
 import { render } from "preact"
 import { Button } from "@material-ui/core"
+import { browser } from "webextension-polyfill-ts"
 
-const App = <Button color="primary">Hello World</Button>
+const App = 
+	<Button color="primary" onClick={() => void browser.runtime.openOptionsPage()}>
+		Open Options
+	</Button>
 
 render(App, document.body)
