0ea82741af1af9cccd9547f1644c39e8ab374812
Eero Lehtinen <eero@eerolehtinen.fi>
2021-07-25 23:31:20 +0300

    Test general options setters

--- tests/background/generalOptions.test.ts
+++ tests/background/generalOptions.test.ts
@@ -1,28 +1,64 @@
-import { BrowserStorage } from "@src/background/browserStorage"
+import { BrowserStorage, StorageSetSuccess } from "@src/background/browserStorage"
 import { GeneralOptions } from "@src/background/generalOptions"
 import { 
 	createDefaultGeneralOptionsData, GeneralOptionsData, 
 } from "@src/background/generalOptionsParser"
-import { ok } from "neverthrow"
+import { ChangedEvent } from "@src/background/observer"
+import { ParseError } from "@src/background/parserHelpers"
+import { err, ok, okAsync } from "neverthrow"
 import { mocked } from "ts-jest/utils"
 
 jest.mock("webextension-polyfill-ts", () => ({}))
 jest.mock("@src/background/browserStorage")
 
-const browserStorageMock = mocked(BrowserStorage, true)
+const mockBrowserStorage = mocked(BrowserStorage, true)
+mockBrowserStorage.prototype.fetchGeneralOptionsData
+	.mockResolvedValue(ok(createDefaultGeneralOptionsData()))
+mockBrowserStorage.prototype.saveGeneralOptionsData
+	.mockReturnValue(okAsync(StorageSetSuccess.Completed))
+
+let browserStorage: BrowserStorage
+
+beforeEach(() => {
+	browserStorage = new BrowserStorage({ preferSync: true })
+})
 
 afterEach(() =>	jest.clearAllMocks())
 
-describe("test BlockSets construction", () => {
-	const browserStorage = new BrowserStorage({ preferSync: true })
+describe("test GeneralOptions construction", () => {
+
 	const testGOData: GeneralOptionsData = {
 		...createDefaultGeneralOptionsData(),
 		typingTestWordCount: 42,
 	}
 	
 	test("loads general settings from browser storage", async() => {
-		browserStorageMock.prototype.fetchGeneralOptionsData.mockResolvedValueOnce(ok(testGOData))
+		mockBrowserStorage.prototype.fetchGeneralOptionsData.mockResolvedValueOnce(ok(testGOData))
 		const generalOptions = await GeneralOptions.create(browserStorage)
 		expect(generalOptions.data).toEqual(testGOData)
+	})
+
+	test("loads default settings if storage returns errors", async() => {
+		mockBrowserStorage.prototype.fetchGeneralOptionsData
+			.mockResolvedValueOnce(err(new ParseError()))
+		const generalOptions = await GeneralOptions.create(browserStorage)
+		expect(generalOptions.data).toEqual(createDefaultGeneralOptionsData())
+	})
+})
+
+describe("test GeneralOptions setters", () => {
+	const changedEventOf = <T>(value: T): ChangedEvent<T> => ({ newValue: value })
+	
+	let generalOptions: GeneralOptions 
+	beforeEach(async() => {
+		generalOptions = await GeneralOptions.create(browserStorage)
+	})
+	
+	test("setTheme", async() => {
+		const listener = jest.fn()
+		generalOptions.subscribeChanged("theme", listener)
+		await generalOptions.setTheme("dark")
+		expect(browserStorage.saveGeneralOptionsData).toBeCalledWith(generalOptions.data)
+		expect(listener).toBeCalledWith(changedEventOf("dark"))
 	})
 })
