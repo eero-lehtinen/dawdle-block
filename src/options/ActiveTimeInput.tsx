7a819d6f3d6444ad9cb0c6f64450ba73549ead0f
Eero Lehtinen <eero@eerolehtinen.fi>
2021-10-26 01:37:04 +0300

    Compose active time input elements

--- src/options/ActiveTimeInput.tsx
+++ src/options/ActiveTimeInput.tsx
@@ -1,9 +1,10 @@
-import { Typography, Box } from "@mui/material"
+import { Typography, Box, Stack } from "@mui/material"
 import { useState } from "preact/hooks"
 import { BlockSet } from "@src/background/blockSet"
 import ValidatingTimerPicker from "./ValidatingTimePicker"
 import { ClockType } from "@src/background/generalOptionsParseTypes"
 import { useEffect } from "react"
+import { HorizontalRuleRounded } from "@mui/icons-material"
 
 interface ActiveTimeInput {
 	blockSet: BlockSet
@@ -29,28 +30,31 @@ const ActiveTimeInput = (props: ActiveTimeInput): JSX.Element => {
 			<Typography variant="h2" sx={{ mb: 1 }}>
 				Active Time
 			</Typography>
-			<ValidatingTimerPicker
-				label={"From time"}
-				inputId={"active-time-from-input"}
-				clockType={clockType}
-				value={activeTime.from}
-				handleValueAccepted={newValue => {
-					blockSet.set("activeTime", { from: newValue, to: activeTime.to })
-					setActiveTime({ from: newValue, to: activeTime.to })
-					onChanged()
-				}}
-			/>
-			<ValidatingTimerPicker
-				label={"To time"}
-				inputId={"active-time-to-input"}
-				clockType={clockType}
-				value={activeTime.to}
-				handleValueAccepted={newValue => {
-					blockSet.set("activeTime", { from: activeTime.from, to: newValue })
-					setActiveTime({ from: activeTime.from, to: newValue })
-					onChanged()
-				}}
-			/>
+			<Stack direction="row" alignItems="center" spacing={2}>
+				<ValidatingTimerPicker
+					label={"From time"}
+					inputId={"active-time-from-input"}
+					clockType={clockType}
+					value={activeTime.from}
+					handleValueAccepted={newValue => {
+						blockSet.set("activeTime", { from: newValue, to: activeTime.to })
+						setActiveTime({ from: newValue, to: activeTime.to })
+						onChanged()
+					}}
+				/>
+				<HorizontalRuleRounded fontSize="medium" />
+				<ValidatingTimerPicker
+					label={"To time"}
+					inputId={"active-time-to-input"}
+					clockType={clockType}
+					value={activeTime.to}
+					handleValueAccepted={newValue => {
+						blockSet.set("activeTime", { from: activeTime.from, to: newValue })
+						setActiveTime({ from: activeTime.from, to: newValue })
+						onChanged()
+					}}
+				/>
+			</Stack>
 		</Box>
 	)
 }
