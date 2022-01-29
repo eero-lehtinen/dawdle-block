e599893c95992955abe3794fb7659ae3353ee840
Eero Lehtinen <eero@eerolehtinen.fi>
2022-01-30 00:34:35 +0200

    Implement rule adding

--- src/options/BlockListInput.tsx
+++ src/options/BlockListInput.tsx
@@ -16,9 +16,13 @@ import {
 	ListItemButton,
 	Box,
 	Typography,
+	TextField,
+	IconButton,
+	NativeSelect,
+	Stack,
 } from "@mui/material"
 import { BlockSet, ListType } from "@src/background/blockSet"
-import { ExpandLessRounded, ExpandMoreRounded } from "@mui/icons-material"
+import { ExpandLessRounded, ExpandMoreRounded, AddRounded } from "@mui/icons-material"
 import { BlockList } from "@src/background/blockSetParseTypes"
 import useEffectCleanUpPageUnload from "@src/shared/useEffectCleanupPageUnload"
 import { ytCategoryNamesById } from "@src/background/constants"
@@ -52,10 +56,17 @@ const BlockRuleItem = (props: DragListItemProps) => {
 
 const listDisplayNames: [keyof BlockList, string][] = [
 	["urlPatterns", "URL Patterns"],
-	["urlRegExps", "URL Regular Expressions"],
 	["ytChannels", "YouTube Channels"],
 	["ytCategoryIds", "YouTube Categories"],
+	["urlRegExps", "URL Regular Expressions"],
 ]
+
+const placeholders = {
+	urlPatterns: "e.g. www.youtube.com*",
+	ytChannels: "e.g. UC2C_jShtL725hvbm1arSV9w",
+	ytCategoryIds: "e.g. 10",
+	urlRegExps: String.raw`e.g. www\.google\.com\/search\?q=(potato|carrot)+`,
+}
 
 interface BlockListCollapseItemProps {
 	blockSet: BlockSet
@@ -164,6 +175,83 @@ const BlockListDraggable = (props: BlockListCollapseItemProps): JSX.Element => {
 	)
 }
 
+interface RuleInputProps {
+	listType: ListType
+	blockSet: BlockSet
+}
+
+const RuleInput = (props: RuleInputProps) => {
+	const { listType, blockSet } = props
+
+	const [ruleType, setRuleType] = useState("urlPatterns")
+
+	const [ruleText, setRuleText] = useState("")
+
+	const addCurrentRule = async () => {
+		let res
+		if (ruleType === "urlPatterns") {
+			res = blockSet.addPattern(listType, ruleText)
+		} else if (ruleType === "ytChannels") {
+			res = await blockSet.addYTChannel(listType, ruleText)
+		} else if (ruleType === "ytCategoryIds") {
+			res = await blockSet.addYTChannel(listType, ruleText)
+		} else if (ruleType === "urlRegExps") {
+			res = await blockSet.addYTChannel(listType, ruleText)
+		} else {
+			throw new Error("Invalid rule!")
+		}
+		if (res.isErr()) {
+			// TODO: error display
+			console.log(`Error adding rule: ${res.error.message}`)
+		} else {
+			setRuleText("")
+		}
+	}
+
+	return (
+		<Box sx={{ px: 1, pb: 1 }}>
+			<Stack direction="row" alignItems="center" spacing={1}>
+				<Typography>Add new rule</Typography>
+				<Box sx={{ p: 1 }}>
+					<NativeSelect
+						value={ruleType}
+						inputProps={{
+							name: "rule type",
+							id: `${listType}-rule-type-select`,
+						}}
+						onChange={event => {
+							setRuleType(event.target.value)
+						}}
+						sx={{ minWidth: 115 }}
+						variant="standard"
+					>
+						<option value="urlPatterns">URL Pattern</option>
+						<option value="ytChannels">YouTube Channel</option>
+						<option value="ytCategoryIds">YouTube Category</option>
+						<option value="urlRegExps">URL RegExp</option>
+					</NativeSelect>
+				</Box>
+			</Stack>
+			<Stack direction="row" alignItems="center" spacing={1}>
+				<TextField
+					id="rule-input"
+					size="small"
+					fullWidth
+					placeholder={placeholders[ruleType as keyof BlockList] ?? "PLACEHOLDER NOT SET"}
+					value={ruleText}
+					onChange={event => setRuleText(event.target.value)}
+					onKeyDown={e => {
+						if (e.key === "Enter") addCurrentRule()
+					}}
+				/>
+				<IconButton aria-label="add rule">
+					<AddRounded onClick={() => addCurrentRule()} />
+				</IconButton>
+			</Stack>
+		</Box>
+	)
+}
+
 interface BlockListInputProps {
 	blockSet: BlockSet
 	listType: ListType
@@ -177,13 +265,12 @@ interface BlockListInputProps {
 const BlockListInput = (props: BlockListInputProps): JSX.Element => {
 	const { blockSet, listType } = props
 
-	// TODO: INPUT
-
 	return (
 		<Box>
 			<Typography variant="h2" sx={{ mb: 1, textTransform: "capitalize" }}>
 				{listType}
 			</Typography>
+			<RuleInput blockSet={blockSet} listType={listType} />
 			{listDisplayNames.map(([key, name]) => (
 				<BlockListDraggable
 					key={key}
@@ -192,7 +279,7 @@ const BlockListInput = (props: BlockListInputProps): JSX.Element => {
 					listKey={key}
 					listName={name}
 					onChanged={() => {
-						/* do nothing */
+						// do nothing
 					}}
 				/>
 			))}
